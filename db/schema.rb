611c8c768fc90b31c1a12def59ff0f7709f1de78
Amanda <ajnikrant@gmail.com>
2021-06-24 13:08:19 -0400

    postgresql backend db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_17_204312) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "advertisements", force: :cascade do |t|
     t.integer "shelter_id"
     t.integer "puppy_id"
